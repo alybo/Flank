40b5d20141294bf6c9223a3f2eb649ee45e3f3b9
alybo <alybo@me.com>
2026-01-13 14:24:35 +0300

    Initial Commit

---
+++ SlideOverTahoe/SlideOverTahoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOverTahoeApp.swift
+//  SlideOverTahoe
+//
+//  Created by Boris Alexandrov on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SlideOverTahoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
