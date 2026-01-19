2f2b5f48b6ec7703ab2070cb4c48f8ea81779464
бо <alybo@me.com>
2026-01-20 02:28:43 +0300

    Draw waist capsule indicator path

--- SlideOverTahoe/EdgeRevealIndicatorController.swift
+++ SlideOverTahoe/EdgeRevealIndicatorController.swift
@@ -100,17 +100,23 @@ final class EdgeRevealIndicatorController: NSObject {
 
     private final class IndicatorView: NSView {
         private let side: DockState.Side
-        private let gradientLayer = CAGradientLayer()
+        private let effectView = NSVisualEffectView()
         private let maskLayer = CAShapeLayer()
+        private let highlightMaskLayer = CAShapeLayer()
         private let borderLayer = CAShapeLayer()
+        private let innerBorderLayer = CAShapeLayer()
+        private let highlightLayer = CAGradientLayer()
         private let chevronLayer = CAShapeLayer()
 
         init(frame frameRect: NSRect, side: DockState.Side) {
             self.side = side
             super.init(frame: frameRect)
             wantsLayer = true
-            layer?.addSublayer(gradientLayer)
+            effectView.autoresizingMask = [.width, .height]
+            addSubview(effectView)
             layer?.addSublayer(borderLayer)
+            layer?.addSublayer(innerBorderLayer)
+            layer?.addSublayer(highlightLayer)
             layer?.addSublayer(chevronLayer)
             configureLayers()
         }
@@ -121,27 +127,41 @@ final class EdgeRevealIndicatorController: NSObject {
 
         override func layout() {
             super.layout()
+            effectView.frame = bounds
             updatePaths()
         }
 
         private func configureLayers() {
-            gradientLayer.colors = [
-                NSColor(white: 0.28, alpha: 0.96).cgColor,
-                NSColor(white: 0.20, alpha: 0.96).cgColor
-            ]
-            gradientLayer.startPoint = CGPoint(x: 0.5, y: 1)
-            gradientLayer.endPoint = CGPoint(x: 0.5, y: 0)
-            gradientLayer.mask = maskLayer
+            effectView.material = .hudWindow
+            effectView.blendingMode = .withinWindow
+            effectView.state = .active
+            effectView.wantsLayer = true
+            effectView.layer?.mask = maskLayer
             maskLayer.fillRule = .evenOdd
             maskLayer.fillColor = NSColor.black.cgColor
 
             borderLayer.fillColor = NSColor.clear.cgColor
-            borderLayer.strokeColor = NSColor.white.withAlphaComponent(0.3).cgColor
-            borderLayer.lineWidth = 1
+            borderLayer.strokeColor = NSColor.white.withAlphaComponent(0.32).cgColor
+            borderLayer.lineWidth = 1.2
             borderLayer.fillRule = .evenOdd
 
+            innerBorderLayer.fillColor = NSColor.clear.cgColor
+            innerBorderLayer.strokeColor = NSColor.white.withAlphaComponent(0.18).cgColor
+            innerBorderLayer.lineWidth = 0.8
+            innerBorderLayer.fillRule = .evenOdd
+
+            highlightLayer.colors = [
+                NSColor.white.withAlphaComponent(0.35).cgColor,
+                NSColor.white.withAlphaComponent(0.0).cgColor
+            ]
+            highlightLayer.startPoint = CGPoint(x: 0.5, y: 1)
+            highlightLayer.endPoint = CGPoint(x: 0.5, y: 0)
+            highlightLayer.mask = highlightMaskLayer
+            highlightMaskLayer.fillRule = .evenOdd
+            highlightMaskLayer.fillColor = NSColor.black.cgColor
+
             chevronLayer.fillColor = NSColor.clear.cgColor
-            chevronLayer.strokeColor = NSColor.black.withAlphaComponent(0.85).cgColor
+            chevronLayer.strokeColor = NSColor.black.withAlphaComponent(0.82).cgColor
             chevronLayer.lineWidth = 5
             chevronLayer.lineCap = .round
             chevronLayer.lineJoin = .round
@@ -149,39 +169,76 @@ final class EdgeRevealIndicatorController: NSObject {
 
         private func updatePaths() {
             let rect = bounds
-            let radius = rect.width / 2
-            let outerRect = rect.insetBy(dx: 0, dy: 8)
-            let bodyPath = CGMutablePath()
-            bodyPath.addRoundedRect(in: outerRect, cornerWidth: radius, cornerHeight: radius)
+            let bodyPath = buildPath(width: rect.width, height: rect.height, wMidRatio: 0.56, wMaxRatio: 0.94, yMaxRatio: 0.38)
 
-            let notchRadius = rect.height * 0.22
-            let notchCenterX = side == .left
-                ? rect.maxX + notchRadius * 0.85
-                : rect.minX - notchRadius * 0.85
-            let notchCenter = CGPoint(x: notchCenterX, y: rect.midY)
-            bodyPath.addEllipse(in: CGRect(x: notchCenter.x - notchRadius,
-                                           y: notchCenter.y - notchRadius,
-                                           width: notchRadius * 2,
-                                           height: notchRadius * 2))
-
-            gradientLayer.frame = rect
             maskLayer.frame = rect
             maskLayer.path = bodyPath
 
             borderLayer.frame = rect
             borderLayer.path = bodyPath
 
+            highlightLayer.frame = rect
+            highlightMaskLayer.frame = rect
+            highlightMaskLayer.path = bodyPath
+
+            let innerInset: CGFloat = 1.2
+            let innerRect = rect.insetBy(dx: innerInset, dy: innerInset)
+            let innerPathBase = buildPath(width: innerRect.width, height: innerRect.height, wMidRatio: 0.56, wMaxRatio: 0.94, yMaxRatio: 0.38)
+            var translation = CGAffineTransform(translationX: innerRect.minX, y: innerRect.minY)
+            let innerPath = innerPathBase.copy(using: &translation) ?? bodyPath
+            innerBorderLayer.frame = rect
+            innerBorderLayer.path = innerPath
+
             let chevronPath = CGMutablePath()
             let chevronWidth: CGFloat = 9
             let chevronHeight: CGFloat = 20
-            let chevronX = notchCenter.x
             let centerY = rect.midY
+            let chevronX = rect.midX
             chevronPath.move(to: CGPoint(x: chevronX + (side == .left ? chevronWidth : -chevronWidth), y: centerY + chevronHeight / 2))
             chevronPath.addLine(to: CGPoint(x: chevronX, y: centerY))
             chevronPath.addLine(to: CGPoint(x: chevronX + (side == .left ? chevronWidth : -chevronWidth), y: centerY - chevronHeight / 2))
 
             chevronLayer.frame = rect
             chevronLayer.path = chevronPath
+        }
+
+        /// buildPath(width:height:wMidRatio:wMaxRatio:yMaxRatio) -> Path
+        /// Example: buildPath(width: 40, height: 92, wMidRatio: 0.56, wMaxRatio: 0.94, yMaxRatio: 0.38)
+        private func buildPath(width: CGFloat, height: CGFloat, wMidRatio: CGFloat, wMaxRatio: CGFloat, yMaxRatio: CGFloat) -> CGPath {
+            let wMax = min(width, width * wMaxRatio)
+            let wMid = min(wMax, width * wMidRatio)
+            let halfMax = wMax / 2
+            let halfMid = wMid / 2
+            let centerX = width / 2
+            let top = CGPoint(x: centerX, y: 0)
+            let bottom = CGPoint(x: centerX, y: height)
+            let yMax = height * yMaxRatio
+            let rightMax = CGPoint(x: centerX + halfMax, y: yMax)
+            let leftMax = CGPoint(x: centerX - halfMax, y: height - yMax)
+
+            let waistY = height / 2
+            let waistRight = CGPoint(x: centerX + halfMid, y: waistY)
+            let waistLeft = CGPoint(x: centerX - halfMid, y: waistY)
+
+            let controlTop = yMax * 0.45
+            let controlBottom = height - (yMax * 0.45)
+
+            let path = CGMutablePath()
+            path.move(to: top)
+            path.addCurve(to: rightMax,
+                          control1: CGPoint(x: centerX, y: controlTop),
+                          control2: CGPoint(x: rightMax.x, y: controlTop))
+            path.addCurve(to: bottom,
+                          control1: CGPoint(x: rightMax.x, y: waistY),
+                          control2: CGPoint(x: waistRight.x, y: controlBottom))
+            path.addCurve(to: leftMax,
+                          control1: CGPoint(x: waistLeft.x, y: controlBottom),
+                          control2: CGPoint(x: leftMax.x, y: waistY))
+            path.addCurve(to: top,
+                          control1: CGPoint(x: leftMax.x, y: controlTop),
+                          control2: CGPoint(x: centerX, y: controlTop))
+            path.closeSubpath()
+            return path
         }
     }
 }
