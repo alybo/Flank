b38ebe32ddb5f8dfe3dcb436928cfa23605415bb
бо <alybo@me.com>
2026-01-20 02:12:55 +0300

    Adjust indicator notch offset

--- SlideOverTahoe/EdgeRevealIndicatorController.swift
+++ SlideOverTahoe/EdgeRevealIndicatorController.swift
@@ -154,8 +154,10 @@ final class EdgeRevealIndicatorController: NSObject {
             let bodyPath = CGMutablePath()
             bodyPath.addRoundedRect(in: outerRect, cornerWidth: radius, cornerHeight: radius)
 
-            let notchRadius = rect.height * 0.22
-            let notchCenterX = rect.midX
+            let notchRadius = rect.height * 0.2
+            let notchCenterX = side == .left
+                ? rect.maxX + notchRadius * 0.35
+                : rect.minX - notchRadius * 0.35
             let notchCenter = CGPoint(x: notchCenterX, y: rect.midY)
             bodyPath.addEllipse(in: CGRect(x: notchCenter.x - notchRadius,
                                            y: notchCenter.y - notchRadius,
